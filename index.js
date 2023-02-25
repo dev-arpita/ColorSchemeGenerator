fc0f5641eeb2de50ff0411f8543bfcfe693f1516
Arpita Roy <arpita.roy.workar@gmail.com>
2023-02-25 11:13:25 +0530

    WIP

--- index.js
+++ index.js
@@ -1,14 +1,34 @@
-fetch("https://www.thecolorapi.com/scheme?hex=0047AB&mode=analogic&count=6")
+const colorPallette = document.getElementById("color-pallette")
+const seedColor = document.getElementById("seed-color").value
+console.log(seedColor)
+const colorPicker = document.getElementById("color-picker")
+const colorMode= document.getElementById("color-mode").value
+const codeIdStore = []
+console.log(colorMode)
+function renderColor() {
+  const hex = seedColor.slice(1,7)
+  console.log("hex",hex)
+  const mode = colorMode
+  fetch(`https://www.thecolorapi.com/scheme?hex=${hex}&mode=${mode}&count=6`)
     .then(res => res.json())
     .then(data => {
-    console.log(data.colors)
-    for(let items in data.seed){
-        console.log(items)
-    // const renderClr = data.map(item => {
-    //     return `
-    //          <div class="box">${item.colors}</div>
-    //     `
-    // })
-    }
-    // document.getElementById("color-pattete").innerHTML=renderClr
-})
+      console.log("data",data)
+      const myColors = data.colors
+      const renderColor = myColors.map(color => {
+              console.log(color.hex.value)
+              console.log(color.name.value)
+              return `
+              <div class="color-box">
+                <div style="background: ${color.hex.value}" class="color">${color.name.value}</div>
+                <div class="color-value">${color.hex.value}</div>
+              </div>
+              `
+          }).join("")
+   colorPallette.innerHTML = renderColor
+    codeIdStore.push(data.color.hex.value)
+    console.log(codeIdStore)
+  })
+   colorPallette.innerHTML = ""
+}
+
+document.getElementById("color-btn").addEventListener("click", renderColor)
