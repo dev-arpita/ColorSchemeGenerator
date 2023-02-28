3883c06eec8f87930c423c9a86c2476eea3d20b8
Arpita Roy <arpita.roy.workar@gmail.com>
2023-02-28 20:06:56 +0530

    wip

--- index.js
+++ index.js
@@ -1,34 +1,24 @@
 const colorPallette = document.getElementById("color-pallette")
-const seedColor = document.getElementById("seed-color").value
-console.log(seedColor)
+const seedColor = document.getElementById("seed-color")
 const colorPicker = document.getElementById("color-picker")
-const colorMode= document.getElementById("color-mode").value
-const codeIdStore = []
-console.log(colorMode)
+const colorMode= document.getElementById("color-mode")
+
 function renderColor() {
-  const hex = seedColor.slice(1,7)
-  console.log("hex",hex)
-  const mode = colorMode
+  const hex = seedColor.value.slice(1,7)
+  const mode = colorMode.value
   fetch(`https://www.thecolorapi.com/scheme?hex=${hex}&mode=${mode}&count=6`)
     .then(res => res.json())
     .then(data => {
-      console.log("data",data)
-      const myColors = data.colors
-      const renderColor = myColors.map(color => {
-              console.log(color.hex.value)
-              console.log(color.name.value)
+      const renderColor = data.colors.map(color => {
               return `
-              <div class="color-box">
-                <div style="background: ${color.hex.value}" class="color">${color.name.value}</div>
+              <div class="color-box flex flex-col">
+                <div style="background: ${color.hex.value}" class="color flex">${color.name.value}</div>
                 <div class="color-value">${color.hex.value}</div>
               </div>
               `
           }).join("")
    colorPallette.innerHTML = renderColor
-    codeIdStore.push(data.color.hex.value)
-    console.log(codeIdStore)
   })
-   colorPallette.innerHTML = ""
 }
 
 document.getElementById("color-btn").addEventListener("click", renderColor)
